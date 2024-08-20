ec4e79e97d04e7e433f2397ede2f0cc17033f822
DiaaHSharqawi <162372579+DiaaHSharqawi@users.noreply.github.com>
2024-08-20 22:41:01 +0300

    MVC_Task_1

---
+++ MVC_Task_1/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Task_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index() // get all
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+      
+    }
+}
